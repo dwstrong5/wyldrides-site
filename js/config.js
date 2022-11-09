3db949e1c88625d162fe15c2dd293e23cd782d32
Don Strong <dstrong230@gmail.com>
2022-11-08 17:56:29 -0800

    changed region in config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_hKGwvG7Fb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5c24ciarmbqf0jmi2bh0s6t6ip', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-west-2
+        region: 'us-west-2' // e.g. us-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
